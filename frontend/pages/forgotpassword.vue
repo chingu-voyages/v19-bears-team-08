a113b00c3c5a421fd6e62cf86cd6919d5a4e2e5e
unknown <danielstrong.ds@gmail.com>
2020-05-18 16:08:36 +0900

    added two other blank pages to not break navigation

---
+++ frontend/pages/forgotpassword.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>This would be the forgot password page</p>
+    <p>This is a sstretch goal</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {},
+})
+</script>
+
+<style></style>
---
+++ frontend/pages/profile.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>This would be the profile page</p>
+    <p>We can slowly add things as the voyage progresses</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {},
+})
+</script>
+
+<style></style>
