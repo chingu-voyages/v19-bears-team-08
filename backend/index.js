3b04cd13e6f1457c9a59dfc33f99b067d35a3baf
unknown <danielstrong.ds@gmail.com>
2020-05-20 09:57:44 +0900

    corrected typos, changed the port, added another mongoose option

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,26 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dovenv');
-//import routes
-const authRoute = require('./routes/auth');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+//import routes
+const authRoute = require("./routes/auth");
+
+const PORT = process.env.PORT || 4000;
 
 dotenv.config();
 
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log('connected to database!')
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("connected to database!")
 );
 
 //Middlewears
 app.use(express.json());
 
 //route middlewears
-app.use('/api/user', authRoute);
+app.use("/api/user", authRoute);
 
-app.listen(3000, () => console.log("server up and running"));
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
