ea69bfa3157e3c706c275c50f00e95c002c3f63c
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2020-05-21 14:44:02 +0900

    CORS Update

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,22 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+// cors
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://chingudashboard.netlify.app", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept, Authorization");
+  next()
+});
+
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+  // Handle the post for this route
+})
+
 //import routes
 const authRoute = require("./routes/auth");
 
