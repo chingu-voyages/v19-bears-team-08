797fdc7080275df5384f93516742153ad910fb34
unknown <danielstrong.ds@gmail.com>
2020-05-22 16:15:39 +0900

    added nuxt auth packages along with their types

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -21,8 +21,30 @@ export default {
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     '@nuxtjs/tailwindcss',
   ],
-  modules: [],
+  modules: ['@nuxtjs/axios', '@nuxtjs/auth'],
+  axios: {
+    baseURL: 'http://localhost:3000/api  ',
+  },
+  auth: {
+    strategies: {
+      local: {
+        endpoints: {
+          login: {
+            url: '/api/auth/login',
+            method: 'post',
+            propertyName: 'token',
+          },
+          logout: { url: '/api/auth/logout', method: 'post' },
+          user: { url: '/api/auth/user', method: 'get', propertyName: 'user' },
+        },
+        // tokenRequired: true,
+        // tokenType: 'bearer',
+        // globalToken: true,
+        // autoFetchUser: true
+      },
+    },
+  },
   build: {
     // extend(config, ctx) {}
   },
-}
+};
