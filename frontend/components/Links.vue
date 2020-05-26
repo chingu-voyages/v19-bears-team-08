d3fca2f9a9721920e0a272a14c1061bdb154e53d
unknown <danielstrong.ds@gmail.com>
2020-05-26 10:26:16 +0900

    updated Links

--- frontend/components/Links.vue
+++ frontend/components/Links.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="flex-auto flex flex-col justify-between">
-    <!-- Other Links -->
+    <!-- Top Links -->
     <ul class="mt-3">
-      <Link v-for="link in links.other" :key="link.to" :link="link" />
+      <Link v-for="link in links.top" :key="link.to" :link="link" />
     </ul>
 
-    <!-- Auth Links -->
+    <!-- Bottom Links -->
     <ul>
-      <Link v-for="link in links.auth" :key="link.to" :link="link" />
+      <Link v-for="link in links.bottom" :key="link.to" :link="link" />
     </ul>
   </div>
 </template>
@@ -23,21 +23,21 @@ import Link from '~/components/Link.vue';
 })
 export default class Links extends Vue {
   links = {
-    auth: [
-      { to: '/login', text: 'Log In', authRequired: false },
-      { to: '/signup', text: 'Sign Up', authRequired: false },
-      { to: '/logout', text: 'Log Out', authRequired: true },
-    ],
-    other: [
+    top: [
       { to: '/', text: 'Home', authRequired: false },
       { to: '/about', text: 'About', authRequired: false },
-      { to: '/profile', text: 'Profile', authRequired: false },
       { to: '/faq', text: 'FAQ', authRequired: false },
       { to: '/pricing', text: 'Pricing', authRequired: false },
       { to: '/schedule', text: 'Schedule', authRequired: false },
       { to: '/howitworks', text: 'How It Works', authRequired: false },
       { to: '/showcase', text: 'Showcase', authRequired: false },
-      { to: '/settings', text: 'Settings', authRequired: false },
+    ],
+    bottom: [
+      { to: '/login', text: 'Log In', authRequired: false },
+      { to: '/signup', text: 'Sign Up', authRequired: false },
+      { to: '/profile', text: 'Profile', authRequired: true },
+      { to: '/settings', text: 'Settings', authRequired: true },
+      { to: '/logout', text: 'Log Out', authRequired: true },
     ],
   };
 }
