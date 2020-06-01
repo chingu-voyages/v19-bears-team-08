7abb16619b76392212bd403ef6d1839e2a52b484
unknown <danielstrong.ds@gmail.com>
2020-06-01 12:43:54 +0900

    added http-errors package, catch all route, and global error function

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,42 @@
 const express = require("express");
+const app = express();
 const mongoose = require("mongoose");
+const createError = require("http-errors");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
-//Constants
-const app = express();
+//constants
 const PORT = process.env.PORT || 4000;
+const NODE_ENV = process.env.NODE_ENV;
 
-//Middlewares
+//middlewares
 app.use(express.json());
 app.use(
   cors({
     allowedHeaders: "X-Requested-With, Content-Type, Accept, Authorization",
-    origin: [
-      "https://chingudashboard.netlify.app",
-      "http://localhost:3000",
-      "mongodb+srv://Snow",
-    ],
+    origin:
+      NODE_ENV === "production"
+        ? "https://chingudashboard.netlify.app"
+        : "http://localhost:3000",
   })
 );
 
-//DB
+//load and set base endpoints here
+const authRoutes = require("./routes/auth");
+app.use("/api/user", authRoutes);
+
+//catches all endpoints that don't exist above
+app.use((req, res, next) => next(createError(404, `Invalid URL: ${req.url}`)));
+
+//catches all errors passed through next()
+app.use((err, req, res, next) => {
+  const status = err.status || 400;
+  const message = err.message || "Unknown Error";
+  res.status(status).json({ message });
+});
+
+//DB connection
 mongoose
   .connect(process.env.DB_CONNECT, {
     useNewUrlParser: true,
@@ -29,9 +44,5 @@ mongoose
   })
   .then(() => console.log("DB established"))
   .catch(error => console.log("fucked up" + JSON.stringify(error)));
-
-//Routes
-const authRoute = require("./routes/auth");
-app.use("/api/user", authRoute);
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
