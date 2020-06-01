91d0c5d51ce7f4995aeb6c3d79f2119452bbc8b0
unknown <danielstrong.ds@gmail.com>
2020-06-01 14:43:55 +0900

    rewrote the verifyToken middleware

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -1,12 +1,31 @@
 const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
 
 module.exports = function verifyToken(req, res, next) {
-  const token = req.header("auth-token");
-  if (!token) return res.status(401).send("Access Denied");
   try {
-    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = verified;
+    const authHeader = req.headers.authorization;
+    if (!authHeader) throw createError(400, "Authorization header is missing");
+
+    const authHeaderStrings = authHeader.split(" ");
+    if (authHeaderStrings[0] !== "Bearer") {
+      throw createError(
+        400,
+        "Authorization header should've started with Bearer"
+      );
+    }
+
+    // this is just the jwt token string
+    const token = authHeaderStrings[1];
+
+    // will throw an error if the JWT isn't valid
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+
+    console.log(decoded);
+    // this will run the next middleware or route controller
+    next();
   } catch (err) {
-    res.status(400).send("Invalid Token");
+    // if the token isn't valid, this runs and our global error ...
+    // ... handler catches this error and sends it to the user
+    next(err);
   }
 };
