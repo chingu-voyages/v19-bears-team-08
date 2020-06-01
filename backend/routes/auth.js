b3fffc7b205d44dc454b461d73416b3b029dfe81
unknown <danielstrong.ds@gmail.com>
2020-06-01 14:43:25 +0900

    update login and register controllers to use global error handler and tested them

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,85 +1,96 @@
 const router = require("express").Router();
+const createError = require("http-errors");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../model/User");
+const verifyToken = require("../middleware/verifyToken");
 const { registerValidation, loginValidation } = require("../utils/validation");
 
 // I prefer this way, so you can easily see all the ...
 // ... endpoints and the middlewares used
 router.post("/register", handleRegistration);
 router.post("/login", handleLogin);
-router.get("/user", getUser);
+router.get("/user", verifyToken, getUser);
 // router.post("/admin", getAdmin);
 // router.post("/dashboard", getUser);
 
-async function handleRegistration(req, res) {
-  console.log(req.body);
-  //LETS VALIDATE THE DATA BEFORE WE ADD A User
-  const { error } = registerValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  //check log if the user is already in the database
-  const emailExist = await User.findOne({ email: req.body.email });
-  if (emailExist) return res.status(400).send("Email already exists");
-
-  // hash the password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-  //Create a new user
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: hashedPassword,
-  });
+async function handleRegistration(req, res, next) {
   try {
-    const savedUser = await user.save();
-    res.json(savedUser);
+    //validate before attempting to create
+    const { error } = registerValidation(req.body);
+    if (error) throw createError(400, error.details[0].message);
+
+    //check log if the user is already in the database
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) throw createError(400, "Email already exists");
+
+    //hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    //create the new user
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    //create jwt token
+    const token = jwt.sign({ userId: newUser._id }, process.env.TOKEN_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(201).json({ token });
   } catch (err) {
-    res.status(400).send(err);
+    next(err);
   }
 }
 
-async function handleLogin(req, res) {
-  const { error } = loginValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+async function handleLogin(req, res, next) {
+  try {
+    const { error } = loginValidation(req.body);
+    if (error) throw createError(400, error.details[0].message);
 
-  const user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("Email or Password is Wrong");
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      throw createError(400, "An account with that email was not found");
+    }
 
-  const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass) return res.status(400).send("Email or Password is Wrong");
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) throw createError(400, "Incorrect password");
 
-  // create and assign a token
-  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
-  res.header("auth-token", token).json(token);
-  // verify it is you, sends back jwt token and userObject
+    // create and assign a token
+    const token = jwt.sign({ userId: user._id }, process.env.TOKEN_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token, user });
+  } catch (err) {
+    next(err);
+  }
 }
 
 // User (returns the user searched for) // logged in users only route
-async function getUser(req, res) {
-  console.log(req.body);
-  // get token from header
-  let token = req.headers.authorization; // Express headers are auto converted to lowercase
-  token = token.slice(7, token.length);
-  console.log(token, testToken);
-  console.log(token == testToken);
-  //check log if the user is in the database
-  const userExists = await User.findOne({ name: req.body.name });
-  if (!userExists) {
-    return res.status(400).send("Cannot find user");
-  } else if (token == testToken) {
-    return res.json(userExists);
-  } else {
-    userExists.email = "****@smith.com";
-    userExists.password = "*****";
-    // send back user data
-    return res.json(userExists);
+async function getUser(req, res, next) {
+  try {
+    const userExists = await User.findOne({ name: req.body.name });
+    if (!userExists) {
+      return res.status(400).send("Cannot find user");
+    } else if (token == testToken) {
+      return res.json(userExists);
+    } else {
+      userExists.email = "****@smith.com";
+      userExists.password = "*****";
+      // send back user data
+      return res.json(userExists);
+    }
+  } catch (err) {
+    next(err);
   }
 }
 
 // // Admin only route (should be logged in and admin)
-// async function getAdmin(req, res) {
+// async function getAdmin(req, res, next) {
 //   console.log(req.body);
 //   if (id !== currentUser.sub && currentUser.role !== Role.Admin) {
 //     return res.status(401).json({ message: "Unauthorized" });
@@ -87,7 +98,7 @@ async function getUser(req, res) {
 // }
 
 // // Dashboard, area you go to once logged in ? // logged in users only !
-// async function getDashboard(req, res) {
+// async function getDashboard(req, res, next) {
 //   console.log(req.body);
 //   let token = req.headers.authorization; // Express headers are auto converted to lowercase
 //   token = token.slice(7, token.length);
