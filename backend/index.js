981b81d2212f4810440ae14dc0d2747a86c342df
unknown <danielstrong.ds@gmail.com>
2020-06-01 11:08:27 +0900

    fixed cors and cleaned main index file up

--- backend/index.js
+++ backend/index.js
@@ -1,48 +1,37 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 const dotenv = require("dotenv");
-
-// cors
-
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://chingudashboard.netlify.app", "http://localhost:3000", "mongodb+srv://Snow");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept, Authorization");
-  next()
-});
-
-app.get('/', function(req, res, next) {
-  // Handle the get for this route
-});
-
-app.post('/', function(req, res, next) {
-  // Handle the post for this route
-})
-
-//import routes
-const authRoute = require("./routes/auth");
-
-const PORT = process.env.PORT || 4000;
-
 dotenv.config();
 
-//Connect to DB
-// mongoose.connect(
-//   process.env.DB_CONNECT,
-//   { useNewUrlParser: true, useUnifiedTopology: true },
-//   err => console.log(err)
-// );
+//Constants
+const app = express();
+const PORT = process.env.PORT || 4000;
 
-mongoose.connect(
-  process.env.DB_CONNECT,
-  { useNewUrlParser: true, useUnifiedTopology: true })
+//Middlewares
+app.use(express.json());
+app.use(
+  cors({
+    allowedHeaders: "X-Requested-With, Content-Type, Accept, Authorization",
+    origin: [
+      "https://chingudashboard.netlify.app",
+      "http://localhost:3000",
+      "mongodb+srv://Snow",
+    ],
+  })
+);
+
+//DB
+mongoose
+  .connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("DB established"))
   .catch(error => console.log("fucked up" + JSON.stringify(error)));
 
-//Middlewears
-app.use(express.json());
-
-//route middlewears
+//Routes
+const authRoute = require("./routes/auth");
 app.use("/api/user", authRoute);
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
