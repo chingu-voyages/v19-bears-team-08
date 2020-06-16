8a39d0775894d2ade71ee3b45e2a26f40755205b
unknown <danielstrong.ds@gmail.com>
2020-06-16 15:50:21 +0900

    updated global styles and typography

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,9 +1,8 @@
 <template>
   <div>
     <div class="app-background app-background-left" />
-    <div class="app-background app-background-right" />
     <div
-      class="main-container min-h-screen max-h-screen ml-auto overflow-hidden bg-gray-200 flex flex-row"
+      class="app-container min-h-screen max-h-screen ml-auto overflow-hidden bg-gray-200 flex flex-row"
     >
       <div
         class="fixed z-20 top-0 w-full h-12 flex justify-between px-2 items-center bg-gray-300 md:hidden"
@@ -28,6 +27,8 @@
           />
         </div>
       </div>
+
+      <!-- Sidebar -->
       <nav
         class="absolute md:static flex flex-col h-full md:h-auto w-64 bg-gray-300 pt-10 md:mt-0 md:py-3 z-10 transition-transform duration-150 transform md:translate-x-0"
         :class="{
@@ -46,11 +47,14 @@
         <Links />
       </nav>
 
+      <!-- Page Content -->
       <main
         class="w-full overflow-y-auto overflow-x-hidden mt-12 md:mt-0 px-2 sm:px-3 md:px-6 py-3 md:py-5 flex flex-col items-center md:items-start md:bg-gray-200 md:opacity-100 transition-all duration-100"
         :class="{ 'opacity-25': isOpen }"
       >
-        <nuxt />
+        <div class="page-container">
+          <nuxt />
+        </div>
       </main>
     </div>
   </div>
@@ -113,9 +117,7 @@ html {
   box-sizing: border-box;
   margin: 0;
 }
-nav {
-  box-shadow: 1px 0 5px 1px #d1d4d8;
-}
+
 .app-background {
   width: 50%;
   height: 100vh;
@@ -125,44 +127,86 @@ nav {
 .app-background-left {
   @apply bg-gray-300 left-0;
 }
-.app-background-right {
-  @apply bg-gray-200 right-0;
-}
-.main-container {
+.app-container {
   max-width: calc(100vw - ((100vw - 1024px) / 2));
   @apply w-full;
 }
-
-main > div {
+.page-container {
+  @apply max-w-screen-md w-full;
+}
+.page-container > div {
   @apply flex flex-col items-center w-full;
 }
-
 @screen md {
-  main > div {
+  .page-container > div {
     @apply items-start;
   }
 }
 
+nav {
+  box-shadow: 1px 0 5px 1px #d1d4d8;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  @apply font-light;
+}
 h1 {
-  @apply text-5xl;
+  @apply text-4xl mb-4;
 }
 h2 {
-  @apply text-4xl;
+  @apply text-3xl mb-3;
 }
 h3 {
-  @apply text-3xl;
+  @apply text-2xl mb-2;
 }
 h4 {
-  @apply text-2xl;
+  @apply text-xl mb-2;
 }
 h5 {
-  @apply text-xl;
+  @apply text-lg mb-1;
 }
 h6 {
-  @apply text-lg;
+  @apply text-base mb-1;
 }
-p {
-  @apply text-gray-800 leading-relaxed;
+@media md {
+  h1 {
+    @apply text-5xl;
+  }
+  h2 {
+    @apply text-4xl;
+  }
+  h3 {
+    @apply text-3xl;
+  }
+  h4 {
+    @apply text-2xl;
+  }
+  h5 {
+    @apply text-xl;
+  }
+  h6 {
+    @apply text-lg;
+  }
+}
+p,
+li,
+a,
+div {
+  @apply text-gray-700 font-normal leading-relaxed mb-1;
+}
+
+* > :first-child {
+  margin-top: 0 !important;
+  margin-left: 0 !important;
+}
+* > :last-child {
+  margin-bottom: 0 !important;
+  margin-right: 0 !important;
 }
 
 .toasted-container .toasted.toasted-msg {
