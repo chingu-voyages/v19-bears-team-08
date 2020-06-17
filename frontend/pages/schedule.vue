4478b3f2154ebbcd07b72e2af9a405cbe54caa4a
unknown <danielstrong.ds@gmail.com>
2020-06-17 14:35:51 +0900

    updated the schedule page styling

--- frontend/pages/schedule.vue
+++ frontend/pages/schedule.vue
@@ -1,9 +1,40 @@
 <template>
-  <div class="container">
-    <h2 class="mb-2">Schedules</h2>
+  <div>
+    <div
+      class="my-6 md:my-8 flex flex-col-reverse md:flex-row md:flex-row items-center"
+    >
+      <div
+        class="flex flex-col items-center md:items-start self-start w-full md:w-3/5 px-2 md:px-0"
+      >
+        <StyledHeader level="h1">Our Schedules</StyledHeader>
+        <p class="mb-6 text-center md:text-left">
+          We offer two types of services: voyages and pair-programming. The
+          former is 6 weeks in length and the latter can be anything from 30
+          minutes to a couple of hours. Below you'll find a breakdown of each
+          service's schedule
+        </p>
+        <div class="flex">
+          <nuxt-link to="/login">
+            <StyledButton green normal>
+              Create an Account
+            </StyledButton>
+          </nuxt-link>
+          <nuxt-link to="/pricing" class="ml-3">
+            <StyledButton pink inverted>
+              View Pricing
+            </StyledButton>
+          </nuxt-link>
+        </div>
+      </div>
+      <img
+        src="/page/schedule/Events.svg"
+        alt="girl looking at a calendar"
+        class="w-8/12 sm:w-1/2 md:w-2/5 ml-0 md:ml-2"
+      />
+    </div>
 
-    <div class="flex flex-col">
-      <h3 class="mb-2">Voyages</h3>
+    <div class="my-6 md:my-8 flex flex-col">
+      <StyledHeader level="h2">Voyages</StyledHeader>
       <blockquote
         class="text-gray-800 py-4 px-8 mt-3 mb-4 mx-0 bg-gray-300 border-pink border-l-4 italic"
       >
@@ -14,8 +45,8 @@
       <StyledList :list="voyageSchedule" />
     </div>
 
-    <div class="flex flex-col">
-      <h3 class="mb-2">Pair Programming</h3>
+    <div class="my-6 md:my-8 flex flex-col">
+      <StyledHeader level="h2">Pair Programming</StyledHeader>
       <blockquote
         class="text-gray-800 py-4 px-8 mt-3 mb-4 mx-0 bg-gray-300 border-pink border-l-4 italic"
       >
@@ -30,10 +61,14 @@
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator';
+import StyledHeader from '~/components/StyledHeader.vue';
+import StyledButton from '~/components/StyledButton.vue';
 import StyledList from '~/components/StyledList.vue';
 
 @Component({
   components: {
+    StyledHeader,
+    StyledButton,
     StyledList,
   },
   head() {
@@ -102,10 +137,12 @@ export default class Schedule extends Vue {
 </script>
 
 <style lang="postcss" scoped>
-.container > div {
-  @apply my-4;
+img {
+  margin-bottom: 1.5rem !important;
 }
-.container > div:last-child {
-  @apply mt-16 mb-0;
+@screen md {
+  img {
+    margin-bottom: 0 !important;
+  }
 }
 </style>
