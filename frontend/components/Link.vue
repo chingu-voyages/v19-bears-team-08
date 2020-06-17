9115633a4b256e09d5820966c57ee9d34f626094
unknown <danielstrong.ds@gmail.com>
2020-06-17 13:51:47 +0900

    made nav links darker

--- frontend/components/Link.vue
+++ frontend/components/Link.vue
@@ -2,7 +2,7 @@
   <li v-if="showLink" class="w-full flex mb-0">
     <nuxt-link
       :to="link.to"
-      class="w-full h-full text-sm sm:text-base p-2 sm:p-3 text-center hover:text-pink"
+      class="w-full h-full text-sm sm:text-base p-2 sm:p-3 text-center text-gray-800 hover:text-pink"
       exactActiveClass="text-pink"
     >
       {{ link.text }}
