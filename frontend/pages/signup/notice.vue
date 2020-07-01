ec9e9a76e5191d76310aa7b74cfbb47de35d7aa2
unknown <danielstrong.ds@gmail.com>
2020-07-01 13:25:26 +0900

    created landing pages for new users

---
+++ frontend/pages/signup/notice.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <StyledHero
+      :header="hero.header"
+      :subText="hero.subText"
+      :imgSrc="hero.imgSrc"
+      :imgAlt="hero.imgAlt"
+    >
+      <a :href="`mailto:${email}`">
+        <StyledButton green normal>
+          Contact Us
+        </StyledButton>
+      </a>
+    </StyledHero>
+  </div>
+</template>
+
+<script>
+import StyledHero from '~/components/StyledHero.vue';
+import StyledButton from '~/components/StyledButton.vue';
+
+export default {
+  name: 'UnverifiedAccount',
+  components: {
+    StyledHero,
+    StyledButton,
+  },
+  asyncData(ctx) {
+    return {
+      email: ctx.env.EMAIL,
+    };
+  },
+  data() {
+    return {
+      email: '',
+      hero: {
+        header: 'Unverified Account',
+        subText: `
+          It looks like you still haven't verified your email, so your account isn't accessible yet.
+          Please check your inbox for an email from us to activate your account.
+          If you never received an email from us, check your spam folder or contact us.
+        `,
+        imgSrc: '/page/signupthanks/WIP.svg',
+        imgAlt: 'work in progress',
+      },
+    };
+  },
+  head() {
+    return {
+      title: 'Unverified | Chingu',
+    };
+  },
+};
+</script>
---
+++ frontend/pages/signup/thanks.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <StyledHero
+      :header="hero.header"
+      :subText="hero.subText"
+      :imgSrc="hero.imgSrc"
+      :imgAlt="hero.imgAlt"
+    >
+      <a :href="`mailto:${email}`">
+        <StyledButton green normal>
+          Contact Us
+        </StyledButton>
+      </a>
+    </StyledHero>
+  </div>
+</template>
+
+<script>
+import StyledHero from '~/components/StyledHero.vue';
+import StyledButton from '~/components/StyledButton.vue';
+
+export default {
+  name: 'SignupThanks',
+  components: {
+    StyledHero,
+    StyledButton,
+  },
+  asyncData(ctx) {
+    return {
+      email: ctx.env.EMAIL,
+    };
+  },
+  data() {
+    return {
+      email: '',
+      hero: {
+        header: 'Welcome to Chingu',
+        subText: `
+          Thanks for creating an account at Chingu.
+          Please check your inbox and verify the email that you signed up with to activate your account.
+          If you haven't received an email within 15 minutes, check your spam folder or contact us.
+        `,
+        imgSrc: '/page/signupthanks/WIP.svg',
+        imgAlt: 'work in progress',
+      },
+    };
+  },
+  head() {
+    return {
+      title: 'Welcome | Chingu',
+    };
+  },
+};
+</script>
