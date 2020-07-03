d544ed0c8da2278648f8e3758ae75b4f2fe45686
dastrong <danielstrong.ds@gmail.com>
2020-07-03 16:38:53 +0900

    updated axios plugin to redirect and provide error messages during auth

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,12 +1,40 @@
-export default function({ $axios }) {
+export default function({ $axios, app }) {
   $axios.interceptors.response.use(
-    response => response,
+    response => {
+      console.log(response.config.url);
+
+      if (process.server) {
+        if (response.config.url === '/user/profile') {
+          app.context.redirect('/');
+          app.$toast.success(`Welcome back, ${response.data.user.name}`);
+        }
+
+        if (response.config.url === '/user/login/github') {
+          app.context.redirect('/');
+          app.$toast.success(`Welcome back, ${response.data.chingu.user.name}`);
+        }
+      }
+      return response;
+    },
     error => {
       const {
         response: {
           data: { message },
         },
       } = error;
+
+      // only want to run these during github logins
+      if (error.config.url === '/user/login/github') {
+        if (process.server) {
+          if (message === 'Please verify your account first') {
+            // if the user hasn't verified their email we'll redirect them to the notice page
+            app.context.redirect('/signup/notice');
+          } else {
+            app.context.redirect('/login');
+          }
+        }
+        app.$toast.error(message);
+      }
 
       return Promise.reject(new Error(message));
     }
