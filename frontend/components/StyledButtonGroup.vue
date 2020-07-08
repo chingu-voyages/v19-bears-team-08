63d1c99035b3cc8b32061eb101a23fed08cc8698
unknown <danielstrong.ds@gmail.com>
2020-07-08 16:12:28 +0900

    extracted button group logic for StyledHero's

---
+++ frontend/components/StyledButtonGroup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="button-group flex flex-wrap justify-center md:justify-start">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="postcss">
+.button-group > * {
+  margin: 5px;
+}
+@screen md {
+  .button-group > * {
+    margin-left: 0px;
+  }
+  .button-group > *:first-child {
+    @apply mr-3;
+  }
+}
+</style>
