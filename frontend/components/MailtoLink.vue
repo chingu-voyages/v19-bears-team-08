f810bea80c472c0837edd69a0ae445d2fbfbc942
unknown <danielstrong.ds@gmail.com>
2020-07-08 16:13:14 +0900

    extracted mailto us logic

---
+++ frontend/components/MailtoLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <a :href="`mailto:${email}`">
+    <slot />
+  </a>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: 'chingucommunity@gmail.com',
+    };
+  },
+};
+</script>
