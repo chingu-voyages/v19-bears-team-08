462adc5420623e9d2809e965f1a24efb894f0955
unknown <danielstrong.ds@gmail.com>
2021-01-14 14:54:02 +0900

    removed deprecated mode config

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 
 export default {
-  mode: 'universal',
-
   env: {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
