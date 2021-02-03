305d480eb599d444bf6c1811f90dbd8fb4f06c2a
DaStrong <danielstrong.ds@gmail.com>
2021-02-03 15:29:49 +0900

    remove the deeply nested destructuring

--- frontend/plugins/axios.ts
+++ frontend/plugins/axios.ts
@@ -1,20 +1,17 @@
 import { Context } from '@nuxt/types';
 import { NuxtAxiosInstance } from '@nuxtjs/axios';
 
-export default function ({ $axios, app }: NuxtAxiosInstance & Context) {
+export default function ({ $axios, redirect }: NuxtAxiosInstance & Context) {
   $axios.interceptors.response.use(
     response => response,
     error => {
-      const {
-        response: {
-          data: { message },
-        },
-      } = error;
+      const { message } = error.response.data;
+
+      // custom routing for specific auth errors
       const msgVerify = `Please verify your account first`;
       const msgNoChingu = `An account was not found with the email that's associated with the provided Github account`;
-      if (message === msgVerify) app.context.redirect('/signup/notice');
-      if (message === msgNoChingu)
-        app.context.redirect('/login/github/not-chingu');
+      if (message === msgVerify) redirect('/signup/notice');
+      if (message === msgNoChingu) redirect('/login/github/not-chingu');
 
       return Promise.reject(new Error(message));
     }
